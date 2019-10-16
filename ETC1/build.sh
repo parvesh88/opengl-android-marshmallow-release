125356b1cc079b634486c466b558cb0cf427e027
Parvesh <parvesh.garg@outlook.com>
2019-10-16 10:24:10 +0530

    Add Files

---
+++ ETC1/build.sh
@@ -0,0 +1,1 @@
+arm-linux-androideabi-gcc -mcpu=cortex-a7 -mfpu=neon-vfpv4 -Wl,--build-id=md5 -shared -o libETC1.so -O2 -fno-exceptions -fno-rtti -s -DLOG_TAG=\"libETC1\" -I../include -lm -fPIC *.cpp
---
+++ build.sh
@@ -0,0 +1,13 @@
+cd EGL
+sh build.sh
+
+cd ../ETC1
+sh build.sh
+
+cd ../GLES_CM
+sh build.sh
+
+cd ../GLES2
+sh build.sh
+
+cd ../
